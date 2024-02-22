a10f6e182a0ca62b7b72c95ac87d8d8ad5ec8cda
Biell <gabrielsouza65115@gmail.com>
2024-02-22 16:04:18 -0300

    feat: Insere a estilização da função Meio para tela mobile

--- page-batman/src/App.jsx
+++ page-batman/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from '/src/Header'
-
+import Meio from '/src/Meio'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <Meio />
     </>
   )
 }
--- page-batman/src/Meio.jsx
+++ page-batman/src/Meio.jsx
@@ -1,22 +1,22 @@
 function Meio() {
     return (
         <main class='bg-black'>
-            <section>
-                <img src="" alt="logo do filme" />
+            <section class='flex flex-col justify-center text-center'>
+                <img src="./src/assets/bat-image.jpg" alt="logo do filme" />
 
-                <div className="btn-box">
-                    <a href="#">Temporada 1</a>
-                    <a href="#">Temporada 2</a>
-                    <a href="#">Temporada 3</a>
+                <div className="btn-box flex flex-col mt-[20px] text-[20px] w-[100%]">
+                    <a class='border-2 border-white ml-[25%] mr-[25%] text-white hover:scale-[1.2] cursor-pointer' href="#">Temporada 1</a>
+                    <a class='mt-[20px] border-2 border-white ml-[25%] mr-[25%] text-white hover:scale-[1.2] cursor-pointer' href="#">Temporada 2</a>
+                    <a class='mt-[20px] border-2 border-white ml-[25%] mr-[25%] text-white hover:scale-[1.2] cursor-pointer' href="#">Temporada 3</a>
                 </div>
 
-                <p>FRASE DO FILME</p>
+                <p class='flex text-center justify-center mt-[20px] text-white '>" Ninguém nunca me deu uma chance. Este é o problema dessa cidade: todo mundo está com medo de se levantar e fazer a coisa certa... " </p>
                 
-                <div btn-box2>
-                    <img src="" alt="botão play"/>
+                <div className="btn-box2 flex justify-center mt-[30px] items-center border-2 bg-white ml-[25%] mr-[25%] rounded-[20px] py-[2%] hover:scale-[1.2] cursor-pointer ">
+                    <img class='size-[20px] mr-[5px] mt-[5px]' src="./src/assets/play-botao.png" alt="botão play"/>
 
                     <div>
-                        <h6>Assistir</h6>
+                        <h6 class='text-[20px]'>Assistir</h6>
                         
                     </div>
                 </div>

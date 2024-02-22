2a68bedb67d51cd295c760fc4ab66f1509317e25
Biell <gabrielsouza65115@gmail.com>
2024-02-22 14:51:13 -0300

    feat: Insere a estilização da função Header para telas de desktop (1024x768)

--- page-batman/src/Header.jsx
+++ page-batman/src/Header.jsx
@@ -1,14 +1,14 @@
 function Header () {
     return (
         <header class='flex flex-row justify-between items-center text-lg md:text-4xl'>
-            <a href="#"><img class='size-[40px] ml-[5px] mt-[5px] md:size-[100px] md:mt-[15px]' src="./src/assets/Morcegos.png" alt="" /></a>
+            <a href="#"><img class='size-[40px] ml-[5px] mt-[5px] md:size-[100px] md:mt-[15px] lg:size-[100px] lg:mt-[1px]' src="./src/assets/Morcegos.png" alt="" /></a>
             <nav className="navigation flex ">
-                <ul class='flex flex-row gap-[20px] items-center md:gap-[40px]'>
+                <ul class='flex flex-row gap-[20px] items-center md:gap-[40px] lg:gap-[70px]'>
                     <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Home</a></li>
                     <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Films</a></li>
                     <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Series</a></li>
                     <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Suport</a></li>
-                    <a class='flex mt-[5px] pr-[10px] md:pr-[20px] hover:text-red-700 transition-[0.2s]' href="#"><ion-icon name="search-outline"></ion-icon></a>
+                    <a class='flex mt-[5px] pr-[10px] md:pr-[20px] lg:pr-[30px] hover:text-red-700 transition-[0.2s]' href="#"><ion-icon name="search-outline"></ion-icon></a>
                     <img class='hidden md:hidden' src="" alt="perfil" />
                 </ul>
             </nav>

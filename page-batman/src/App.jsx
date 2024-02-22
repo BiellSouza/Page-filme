aa5553b541d119834c871cf793ef12fd6ffbf1aa
Biell <gabrielsouza65115@gmail.com>
2024-02-22 14:36:30 -0300

    feat: Insere a estilização da função Header para tela mobile (360x640)

--- page-batman/src/App.jsx
+++ page-batman/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Header from '/src/Header'
 
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
--- page-batman/src/Header.jsx
+++ page-batman/src/Header.jsx
@@ -1,15 +1,15 @@
 function Header () {
     return (
-        <header>
-            <a href="#"><img src="" alt="" /></a>
-            <nav className="navigation">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Films</a></li>
-                    <li><a href="#">Series</a></li>
-                    <li><a href="#">Suport</a></li>
-                    <img src="" alt="search" />
-                    <img src="" alt="perfil" />
+        <header class='flex flex-row justify-between items-center text-lg'>
+            <a href="#"><img class='size-[40px] ml-[5px] mt-[5px]' src="./src/assets/Morcegos.png" alt="" /></a>
+            <nav className="navigation flex ">
+                <ul class='flex flex-row gap-[20px] items-center'>
+                    <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Home</a></li>
+                    <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Films</a></li>
+                    <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Series</a></li>
+                    <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Suport</a></li>
+                    <a class='flex mt-[5px] pr-[10px] hover:text-red-700 transition-[0.2s]' href="#"><ion-icon name="search-outline"></ion-icon></a>
+                    <img class='hidden' src="" alt="perfil" />
                 </ul>
             </nav>
         </header>

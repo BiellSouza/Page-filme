876ca07be3993b5987488bb7d39fbc45f76a656c
Biell <gabrielsouza65115@gmail.com>
2024-02-22 14:42:59 -0300

    feat: Insere a estilização da função Header para telas de tablet (768x1024)

--- page-batman/src/Header.jsx
+++ page-batman/src/Header.jsx
@@ -1,15 +1,15 @@
 function Header () {
     return (
-        <header class='flex flex-row justify-between items-center text-lg'>
-            <a href="#"><img class='size-[40px] ml-[5px] mt-[5px]' src="./src/assets/Morcegos.png" alt="" /></a>
+        <header class='flex flex-row justify-between items-center text-lg md:text-4xl'>
+            <a href="#"><img class='size-[40px] ml-[5px] mt-[5px] md:size-[100px] md:mt-[15px]' src="./src/assets/Morcegos.png" alt="" /></a>
             <nav className="navigation flex ">
-                <ul class='flex flex-row gap-[20px] items-center'>
+                <ul class='flex flex-row gap-[20px] items-center md:gap-[40px]'>
                     <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Home</a></li>
                     <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Films</a></li>
                     <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Series</a></li>
                     <li><a class='hover:text-red-700 transition-[0.2s]' href="#">Suport</a></li>
-                    <a class='flex mt-[5px] pr-[10px] hover:text-red-700 transition-[0.2s]' href="#"><ion-icon name="search-outline"></ion-icon></a>
-                    <img class='hidden' src="" alt="perfil" />
+                    <a class='flex mt-[5px] pr-[10px] md:pr-[20px] hover:text-red-700 transition-[0.2s]' href="#"><ion-icon name="search-outline"></ion-icon></a>
+                    <img class='hidden md:hidden' src="" alt="perfil" />
                 </ul>
             </nav>
         </header>

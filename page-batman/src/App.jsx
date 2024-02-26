71c70192c8368cfbadbc25464e9bab19135e2139
Gabriel <gabrielsouza65115@gmail.com>
2024-02-26 20:13:36 -0300

    feat: Insere a estilização da página inteira (Header, Meio e Footer) para telas até 360

--- page-batman/src/App.jsx
+++ page-batman/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from '/src/Header'
 import Meio from '/src/Meio'
+import Footer from '/src/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Meio />
+      <Footer />
     </>
   )
 }
